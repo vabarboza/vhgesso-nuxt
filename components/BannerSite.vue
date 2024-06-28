bc23eed6be34a29068f7bea0277e8fb0eddf33a3
Vinicius de Araujo Barboza <dev.viniciusbarboza@gmail.com>
2024-06-28 12:20:21 -0300

    Ajustes na ViewPort

--- components/BannerSite.vue
+++ components/BannerSite.vue
@@ -3,9 +3,11 @@
         <section class="hero is-small hero-background">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <img class="title has-text-white" style="height: 200px" src="~/assets/images/logo.png" />
-                    <p class="subtitle has-text-dark is-5 mt-3">Forros - Molduras - Gesso Acartonado</p>
-                    <p class="subtitle has-text-dark is-5 margin-top">Divisorias - Sancas - Colunas - Arte em Gesso</p>
+                    <img class="title has-text-white responsive-image" src="~/assets/images/logo.png" />
+                    <p class="subtitle has-text-dark is-5 has-text-weight-semibold mt-3">Forros - Molduras - Gesso
+                        Acartonado</p>
+                    <p class="subtitle has-text-dark is-5 has-text-weight-semibold margin-top">Divisorias - Sancas -
+                        Colunas - Arte em Gesso</p>
                 </div>
             </div>
         </section>
@@ -25,7 +27,35 @@ export default {
     background-position: center;
 }
 
+.responsive-image {
+    height: 10vw;
+    /* Ajuste o valor conforme necessário */
+}
+
+@media (max-width: 768px) {
+
+    /* Para dispositivos móveis */
+    .responsive-image {
+        height: 20vw;
+        /* Ajuste o valor conforme necessário */
+    }
+}
+
+.subtitle {
+    font-size: 2vw;
+    /* Ajuste o valor conforme necessário */
+}
+
+@media (max-width: 768px) {
+
+    /* Para dispositivos móveis */
+    .subtitle {
+        font-size: 3vw !important;
+        /* Ajuste o valor conforme necessário */
+    }
+}
+
 .margin-top {
-    margin-top: -30px;
+    margin-top: -25px;
 }
 </style>
